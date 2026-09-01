a93f3e1d446adf5759e7af1abdba6d098db87b3a
heinricov abraham <heinricoov@gmail.com>
2026-09-01 14:35:33 +0700

    publish package : 0.0.1

--- packages/docsman/src/index.ts
+++ packages/docsman/src/index.ts
@@ -1,1 +1,14 @@
 "use client"
+
+export { cn } from "./lib/utils"
+export { DocsmanLayout } from "./layouts/docsman-layout"
+export type {
+  LayoutProps,
+} from "./types/layouts"
+export type {
+  MenuSectionProps,
+  MenusProps,
+  MenuItemProps,
+  GrupProps,
+  SosmedProps,
+} from "./types/menus"
