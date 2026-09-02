e53002019d823a4f50679b702ea309464983d4e7
heinricov abraham <heinricoov@gmail.com>
2026-09-02 20:55:49 +0700

    update - docs : mwnus

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -47,33 +47,66 @@ export const menus = [
         href: "/docs/getstarted",
       },
       {
+        title: "Init",
+        href: "/docs/init",
+      },
+      {
         title: "Use Templates",
+        href: "/docs/templates",
+      },
+      {
+        title: "Deploy",
         href: "/docs/templates",
       },
     ],
   },
   {
-    type: "NavCollaps",
+    type: "NavMenus",
+    label: "Components",
     menus: [
       {
-        title: "Components",
-        url: "#",
-        icon: <Component />,
-        isActive: true,
-        items: [
-          {
-            title: "Command Code",
-            url: "/docs/components/command-code",
-          },
-          {
-            title: "Code Block",
-            url: "/docs/components/code-block",
-          },
-          {
-            title: "Table Props",
-            url: "/docs/components/table-props",
-          },
-        ],
+        title: "Alert Section",
+        href: "/docs/components/alert-section",
+      },
+      {
+        title: "Card Section",
+        href: "/docs/components/card-section",
+      },
+      {
+        title: "Code Block",
+        href: "/docs/components/code-block",
+      },
+      {
+        title: "Command Code",
+        href: "/docs/components/command-code",
+      },
+      {
+        title: "Componen Preview",
+        href: "/docs/components/component-preview",
+      },
+      {
+        title: "File Tree",
+        href: "/docs/components/file-tree",
+      },
+      {
+        title: "Logo Section",
+        href: "/docs/components/logo-section",
+      },
+      {
+        title: "Steps Section",
+        href: "/docs/components/steps-section",
+      },
+      {
+        title: "Table Props",
+        href: "/docs/components/table-props",
+      },
+      {
+        title: "Terminal View",
+        href: "/docs/components/terminal-view",
+      },
+      {
+        title: "Typograpy",
+        href: "/docs/components/typograpy",
       },
     ],
   },
